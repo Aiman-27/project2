4992e0bd02847137ecfb3939b684d9a2590543bd
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-03-21 06:50:28 -0700

    Update index.js
    index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ass1 from './Ass1';
 import reportWebVitals from './reportWebVitals';
 
 /*
